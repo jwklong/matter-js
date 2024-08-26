9153549e71dee84fb0cd300a212ab3e6477bec3c
sussy layers dev <jwklong@gmx.com>
2024-08-26 20:17:02 +0000

    blah

--- build/matter.js
+++ build/matter.js
@@ -1631,6 +1631,7 @@ var Axes = __webpack_require__(11);
             position: { x: 0, y: 0 },
             force: { x: 0, y: 0 },
             torque: 0,
+            gravityScale: 0,
             positionImpulse: { x: 0, y: 0 },
             constraintImpulse: { x: 0, y: 0, angle: 0 },
             totalContacts: 0,
@@ -2339,8 +2340,8 @@ var Axes = __webpack_require__(11);
             velocityPrevY = (body.position.y - body.positionPrev.y) * correction;
 
         // update velocity with Verlet integration
-        body.velocity.x = (velocityPrevX * frictionAir) + (body.force.x / body.mass) * deltaTimeSquared;
-        body.velocity.y = (velocityPrevY * frictionAir) + (body.force.y / body.mass) * deltaTimeSquared;
+        body.velocity.x = (velocityPrevX * frictionAir) + (body.force.x / body.mass) * deltaTimeSquared * body.gravityScale;
+        body.velocity.y = (velocityPrevY * frictionAir) + (body.force.y / body.mass) * deltaTimeSquared * body.gravityScale;
 
         body.positionPrev.x = body.position.x;
         body.positionPrev.y = body.position.y;
